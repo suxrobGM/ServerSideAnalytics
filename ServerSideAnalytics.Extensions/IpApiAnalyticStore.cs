ae35b590d91cb580da073a9807dd20873d70ce04
Matteo Fabbri <matteo@phascode.org>
2018-07-10 23:01:25 +0200

    nuget update

--- ServerSideAnalytics.Extensions/IpApiAnalyticStore.cs
+++ ServerSideAnalytics.Extensions/IpApiAnalyticStore.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
-using Maddalena;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
--- ServerSideAnalytics.Extensions/IpInfoAnalyticStore.cs
+++ ServerSideAnalytics.Extensions/IpInfoAnalyticStore.cs
@@ -4,7 +4,6 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using Maddalena;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
